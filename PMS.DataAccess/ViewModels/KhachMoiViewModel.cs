cc3333e9cd685fbc4c88860b81da0bfe204a8c4b
dotranquangphuc <dotranquangphuc@gmail.com>
2016-05-11 22:57:07 +0700

    Add new KhachMoiViewModel

---
+++ PMS.DataAccess/ViewModels/KhachMoiViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.DataAccess.ViewModels
+{
+    public class KhachMoiViewModel
+    {
+        public int id { get; set; }
+        public string HoTen { get; set; }
+        public string ChucDanh { get; set; }
+        public string DiaChi { get; set; }
+        public string SoDienThoai { get; set; }
+        public string Email { get; set; }
+        public string GhiChu { get; set; }
+        public int GiayMoiId { get; set; }
+       
+    }
+}
