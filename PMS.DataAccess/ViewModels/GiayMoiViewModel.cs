c456642d6bdedbed9fd2b0d0d718fb6552ce7fa7
dotranquangphuc <dotranquangphuc@gmail.com>
2016-05-11 22:55:57 +0700

    Add new GiayMoiViewModel

---
+++ PMS.DataAccess/ViewModels/GiayMoiViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.DataAccess.ViewModels
+{
+    public class GiayMoiViewModel
+    {
+        public int id
+        {
+            get; set;
+
+        }
+
+        public string Ten
+        {
+            get; set;
+        }
+        public int Loai
+        {
+            get; set;
+        }
+        public string Mau
+        {
+            get; set;
+        }
+        public string NgayMoi
+        {
+            get; set;
+        }
+        public string NgaySuKien
+        {
+            get; set;
+        }
+        public string DiaDiem
+        {
+            get; set;
+        }
+        public int? NguoiGoi
+        {
+            get; set;
+        }
+       public string ThoiGian
+        {
+            get; set;
+        }
+        public int? MauId
+        {
+            get; set;
+        }
+
+        
+    }
+}
