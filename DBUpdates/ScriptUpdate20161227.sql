9c23dda66911ef62a9bd18befef7b94108ce6d97
baotoan93 <3114793@gmail.com>
2017-01-04 14:14:25 +0700

    Update ScriptUpdate20161227.sql

--- DBUpdates/ScriptUpdate20161227.sql
+++ DBUpdates/ScriptUpdate20161227.sql
@@ -1,4 +1,4 @@
-INSERT [dbo].[Menu] ([Id], [ScreenId], [Text], [NewWindow], [IconClass], [ParentId], [Position], [Visible], [MenuType], [Link]) VALUES (1, N'/VocationFilter/Index', N'Thông tin giáo dân', 0, N'fa-users', 0, 1200, 1, 1, N'/VocationFilter/Index')
+INSERT [dbo].[Menu] ([Id], [ScreenId], [Text], [NewWindow], [IconClass], [ParentId], [Position], [Visible], [MenuType], [Link]) VALUES (61, N'/VocationFilter/Index', N'Thông tin giáo dân', 0, N'fa-users', 0, 1200, 1, 1, N'/VocationFilter/Index')
 INSERT [dbo].[ScreenAction] ([Id], [Name], [Enabled]) VALUES (N'/VocationFilter/Index', N'Thông tin giáo dân', 1)
 INSERT [dbo].[RolePermission] ([ScreenId], [RoleId], [Permission]) VALUES (N'/VocationFilter/Index', 1, 1)
 
